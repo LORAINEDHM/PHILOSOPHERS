f42070c5486ff3cbab92b299b7e37ad2a49e8412
Loraine Duhamel <lduhamel@shi-r10-p4.s19.be>
2021-09-28 16:32:09 +0200

    cleaned up one philo

--- philosopher/srcs/actions.c
+++ philosopher/srcs/actions.c
@@ -30,31 +30,19 @@ int    ph_take_fork(t_philo *ph, t_indiv *p)
 		pthread_mutex_unlock(&ph->forks[p->l_fork - 1]);
 		return (0);
 	}
-	
-	if (ph->n_philo > 1)
+	p->r_fork = p->id;
+	pthread_mutex_lock(&ph->forks[p->r_fork - 1]);
+	if ((!(ph_write_message(ph, p, "has taken a fork\n"))) ||
+		(!(ph_write_message(ph, p, "is eating\n"))))
 	{
-		p->r_fork = p->id;
-		pthread_mutex_lock(&ph->forks[p->r_fork - 1]);
-		if ((!(ph_write_message(ph, p, "has taken a fork\n"))) ||
-			(!(ph_write_message(ph, p, "is eating\n"))))
-		{
-			pthread_mutex_unlock(&ph->forks[p->l_fork - 1]);
-			pthread_mutex_unlock(&ph->forks[p->r_fork - 1]);
-			return (0);
-		}
-		ph->last_meals[p->id - 1][0] = ph_get_time_today(&ph->tv);
-		ph_usleep(p->msg_usec, ph->eat_time * 1000);
 		pthread_mutex_unlock(&ph->forks[p->l_fork - 1]);
 		pthread_mutex_unlock(&ph->forks[p->r_fork - 1]);
+		return (0);
 	}
-	// else
-	// {
-	// 	if (ph->died == 1)
-	// 	{
-	// 		pthread_mutex_unlock(&ph->forks[p->l_fork - 1]);
-	// 		return(0);
-	// 	}
-	// }
+	ph->last_meals[p->id - 1][0] = ph_get_time_today(&ph->tv);
+	ph_usleep(p->msg_usec, ph->eat_time * 1000);
+	pthread_mutex_unlock(&ph->forks[p->l_fork - 1]);
+	pthread_mutex_unlock(&ph->forks[p->r_fork - 1]);
 	return (1);
 }
 
